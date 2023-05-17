15f9ca1b452d0e6c96a595620eead4f0309964d3
prince-spooler <132440531+prince-spooler@users.noreply.github.com>
2023-05-17 20:40:44 +0800

    Update Unquoted-service-path-test-1.ps1

--- Unquoted-service-path-test-1.ps1
+++ Unquoted-service-path-test-1.ps1
@@ -35,7 +35,7 @@ foreach ($path in $servicePaths) {
                 # If the service is not running, restart the system
                 $svcStatus = Get-Service -DisplayName "TRIGONE Remote System Monitor Service" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Status
                 if ($svcStatus -ne "Running") {
-                    # Restart-Computer -Force -ErrorAction SilentlyContinue
+                     Restart-Computer -Force -ErrorAction SilentlyContinue
                 }
                 break
             }
